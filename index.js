c70fe89735770256f1e4a8e97c637609c3c4a853
Kats00 <ksoohgt3@gmail.com>
2024-03-14 23:02:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
 app.use('/', require('../routes/userRoutes'))
 
 app.get('/', (req, res) => {
